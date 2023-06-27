8af02cb19417eac776000dc4947a71e6a0aca06d
lucomoro <morelli.2000luca@gmail.com>
2023-06-27 13:14:18 +0200

    Fix homepage redirection

--- db_project/app/urls.py
+++ db_project/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('', views.home),
-    path ('contattaci', views.contattaci),
+    path ('', views.home, name='home'),
+    path ('contattaci', views.contattaci, name="contattaci"),
 ]
